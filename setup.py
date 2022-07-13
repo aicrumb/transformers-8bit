2e3dba1019ef31ac1fdc587bc8326c61e3466a44
aicrumb <58605641+aicrumb@users.noreply.github.com>
2022-07-13 10:54:00 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='gptj6b',
+    version='0.0.1',
+    author='aicrumb',
+    author_email='crumby.naomi@gmail.com',
+    description='code to load gpt-j-6b in 8bit',
+    long_description='i was bored of writing the code myself every time lol',
+    long_description_content_type="text/markdown",
+    url='https://github.com/aicrumb/gpt-j-8bit',
+    project_urls = {
+        "Bug Tracker": "https://github.com/aicrumb/gpt-j-8bit/issues"
+    },
+    license='GNU GPLv3',
+    packages=['gptj6b'],
+    install_requires=['transformers', 'datasets'],
+)
